38710cb8a6ab4475d95c8f69cd80727ae5a83aa2
Sean Murphy <sean@iamseanmurphy.com>
2025-06-09 10:17:57 +0000

    1.47.0

--- yith-woocommerce-featured-video/init.php
+++ yith-woocommerce-featured-video/init.php
@@ -3,17 +3,18 @@
  * Plugin Name: YITH WooCommerce Featured Video
  * Plugin URI: https://yithemes.com/themes/plugins/yith-woocommerce-featured-audio-video-content/
  * Description: <code><strong>YITH WooCommerce Featured Video</strong></code> allows you to set a video or audio instead of the featured image on the single product page. <a href ="https://yithemes.com">Get more plugins for your e-commerce shop on <strong>YITH</strong></a>
- * Version: 1.46.0
+ * Version: 1.47.0
  * Author: YITH
  * Author URI: https://yithemes.com/
  * Text Domain: yith-woocommerce-featured-video
  * Domain Path: /languages/
- * WC requires at least: 9.6
- * WC tested up to: 9.8
+ * WC requires at least: 9.7
+ * WC tested up to: 9.9
+ *  Requires Plugins: woocommerce
  *
  * @author YITH <plugins@yithemes.com>
  * @package YITH WooCommerce Featured Audio Video Content
- * @version 1.46.0
+ * @version 1.47.0
  */
 
 /*
@@ -68,7 +69,7 @@ function yith_ywcfav_install_free_admin_notice() {
 }
 
 if ( ! defined( 'YWCFAV_VERSION' ) ) {
-	define( 'YWCFAV_VERSION', '1.44.0' );
+	define( 'YWCFAV_VERSION', '1.47.0' );
 }
 
 if ( ! defined( 'YWCFAV_FREE_INIT' ) ) {
--- yith-woocommerce-featured-video/readme.txt
+++ yith-woocommerce-featured-video/readme.txt
@@ -5,7 +5,7 @@ Tags: woocommerce, featured video, featured image, featured, product, e-commerce
 Requires PHP: 7.4
 Requires at least: 6.6
 Tested up to: 6.8
-Stable tag: 1.46.0
+Stable tag: 1.47.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -76,6 +76,10 @@ You can report security bugs through the Patchstack Vulnerability Disclosure Pro
 2. The admin field in product configuration.
 
 == Changelog ==
+
+= 1.47.0 - Released on 09 June 2025 =
+* New: Support for WooCommerce 9.9
+* Update: YITH plugin framework
 
 = 1.46.0 - Released on 03 April 2025 =
 * New: Support for WordPress 6.8
